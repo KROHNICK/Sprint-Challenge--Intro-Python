534feef4727db31d6693f458bba2e95342e0b3c0
KROHNICK <business.kanekperez@gmail.com>
2019-04-24 14:12:46 -0700

    passed all cityreader tests except correctness

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -41,7 +41,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(f"{c.name}: ({c.latitude},{c.longitude})")
+    print(c.name, c.latitude, c.longitude)
 
 # STRETCH GOAL!
 #
